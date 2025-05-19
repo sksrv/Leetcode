fda2a15132d97a4b8c578b7869ddc887dbc49687
Sourav <145606223+sksrv@users.noreply.github.com>
2025-05-19 23:32:25 +0530

    Time: 3 ms (3.25%), Space: 7.9 MB (3.89%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.cpp
+++ 2083-three-divisors/2083-three-divisors.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool isThree(int n) {
-        int i=1 , count=1;
-        while(i<=n/2){
+        int count=1;
+        for(int i=1;i<=n/2;i++){
             if(n%i==0){
                 count++;
-            }
-            if(count>3){
-                return false;
-            }
-            if(i*i==n){
-                return true;
-            }
-            i++;
+            };
+        }
+        if(count==3){
+            return true;
         }
         return false;
     }
