0fb5b1d28d430337dc04d53c1a456a41554e98a6
Sourav <145606223+sksrv@users.noreply.github.com>
2025-08-07 18:36:31 +0530

    Time: 7 ms (60.25%), Space: 128.2 MB (79.43%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -4,7 +4,7 @@ public:
         int posIndex = 0;
         int negIndex = 1;
         int n = nums.size();
-        vector<int> ans(n);
+        vector<int> ans(n,0);
 
         for(int i=0;i<n;i++){
             if(nums[i]>0){
