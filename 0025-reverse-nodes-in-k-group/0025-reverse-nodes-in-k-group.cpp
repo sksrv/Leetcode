d02ab96d799260be4e4a64de580ddfae617905b3
Sourav <145606223+sksrv@users.noreply.github.com>
2025-09-02 19:20:54 +0530

    Time: 0 ms (100%), Space: 16.3 MB (96.04%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -11,28 +11,38 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
+        //base case
+        if(head == NULL){
+            return NULL;
+        }
+        //Step0: check atleast k-Nodes are available
         ListNode* temp = head;
-        int cnt = 0;
-
-        while(cnt<k){
-            if(temp == NULL)   return head;
-
+        int length = 0;
+        while (temp != NULL && length < k) {
             temp = temp->next;
-            cnt++;
+            length++;
         }
-
-        ListNode* prevNode = reverseKGroup(temp, k);
-
-        temp = head; cnt = 0;
-        while(cnt < k){
-            ListNode* next = temp->next;
-            temp->next = prevNode;
-
-            prevNode = temp;
-            temp = next;
-            cnt++;
+        if (length < k) {
+            return head;
         }
+        //Step1: Reverse first k Nodes
+        ListNode* forward = NULL;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        int count = 0;
 
-        return prevNode;
+        while(curr != NULL && count < k){
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+            count++;
+        }
+        //Step2: Recursion will handle next
+        if(forward != NULL){
+            head->next = reverseKGroup(forward,k);
+        }
+        //Step3: Return Head of reversed List
+        return prev;
     }
 };
