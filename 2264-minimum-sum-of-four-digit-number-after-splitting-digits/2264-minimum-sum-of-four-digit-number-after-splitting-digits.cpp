d57616b8a52c88b2c1784f94fadbe5710a9ca001
Sourav <145606223+sksrv@users.noreply.github.com>
2025-09-04 17:03:20 +0530

    Time: 0 ms (100%), Space: 8 MB (21.96%) - LeetHub

--- 2264-minimum-sum-of-four-digit-number-after-splitting-digits/2264-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/2264-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -1,25 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int minimumSum(int num) {
-        int ones = num%10;
-        num /=10;
-        int tens = num%10;
-        num /=10;
-        int hun  =  num%10;
-        num /=10;
-        int thou = num%10;
         
-        if (ones > tens) swap(ones, tens);
-        if (ones > hun) swap(ones, hun);
-        if (ones > thou) swap(ones, thou);
-        if (tens > hun) swap(tens, hun);
-        if (tens > thou) swap(tens, thou);
-        if (hun > thou) swap(hun, thou);
+        vector<int> digits;
+        for (int i = 0; i < 4; i++) {
+            digits.push_back(num % 10);
+            num /= 10;
+        }
+        
+        sort(digits.begin(), digits.end());
 
-        int new1 = ones*10 + thou;
-        int new2 = tens*10 + hun;
+        int new1 = digits[0] * 10 + digits[3]; 
+        int new2 = digits[1] * 10 + digits[2]; 
 
-        return new1+new2;
-   
+        return new1 + new2;
     }
 };
