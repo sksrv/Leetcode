58987a03f477bafbbf1ef7d5df7657a1a4875f68
Sourav <145606223+sksrv@users.noreply.github.com>
2025-09-07 13:46:18 +0530

    Time: 0 ms (100%), Space: 16.1 MB (34.84%) - LeetHub

--- 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.cpp
+++ 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.cpp
@@ -1,27 +1,23 @@
+#include <vector>
+#include <numeric> // for accumulate
+using namespace std;
+
 class Solution {
 public:
     int findMiddleIndex(vector<int>& nums) {
-        int leftSum;
-        int rightSum=0;
-        for(int i=0;i<nums.size();i++){
-            if(i==0){
-                leftSum=0;
-                rightSum = std::accumulate(nums.begin()+1, nums.end(), 0);
-                if(leftSum==rightSum){
-                    return i;
-                }
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        int leftSum = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if(leftSum == rightSum) {
+                return i; 
             }
-            if(i==nums.size()-1){
-                leftSum=std::accumulate(nums.begin(), nums.end()-1, 0);
-                rightSum =0;
-                if(leftSum==rightSum){
-                    return i;
-                }
-            }
-if(accumulate(nums.begin(), nums.begin()+i, 0)==accumulate(nums.begin()+i+1, nums.end(), 0)){
-                return i;
-            }
+
+            leftSum += nums[i]; 
         }
-        return -1;
+
+        return -1; 
     }
 };
