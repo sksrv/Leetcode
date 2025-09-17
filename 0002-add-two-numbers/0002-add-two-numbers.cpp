a47e0cec4a1f39dc62ae5f4d10f33e9d7eea0b59
Sourav <145606223+sksrv@users.noreply.github.com>
2025-09-17 17:47:59 +0530

    Time: 1 ms (46.56%), Space: 77.3 MB (13.42%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+  
+    //InsertNodes At Tail
+    void insertAtTail(ListNode*&head, ListNode* &tail, int val){
+        ListNode* temp = new ListNode(val);
+        if(head == NULL){
+            head = temp;
+            tail = temp;
+            return;
+        }
+        else{
+            tail->next = temp;
+            tail = temp;
+            return;
+        }
+    }
+    //add Numbers
+    ListNode* add(ListNode* l1, ListNode* l2){
+        int carry = 0;
+
+        ListNode* ansHead = NULL;
+        ListNode* ansTail = NULL;
+
+        while(l1 != NULL && l2 != NULL){
+            int sum = carry + l1->val + l2->val;
+            int digit = sum%10;
+
+            insertAtTail(ansHead, ansTail, digit);
+
+            carry = sum / 10;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+    
+        while(l1 != NULL){
+            int sum = carry + l1->val;
+            int digit = sum%10;
+
+            insertAtTail(ansHead, ansTail, digit);
+            
+            carry = sum / 10;
+            l1 = l1->next;
+        }
+        while(l2 != NULL){
+            int sum = carry + l2->val;
+            int digit = sum%10;
+
+            insertAtTail(ansHead, ansTail, digit);
+            
+            carry = sum / 10;
+            l2 = l2->next;
+        }
+        while(carry != 0){
+            int sum = carry;
+
+            insertAtTail(ansHead, ansTail, sum);
+            carry = carry / 10;
+        }
+        return ansHead;
+    }
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+       
+        ListNode* ans = add(l1, l2);
+
+        return ans;
+    }
+};
