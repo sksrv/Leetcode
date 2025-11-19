ee29a1f1cc9a725cc408d174d1154a3349bd5f63
Sourav <145606223+sksrv@users.noreply.github.com>
2025-11-20 00:03:45 +0530

    Time: 0 ms (100%), Space: 14.1 MB (68.93%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
-        auto it = std::find(nums.begin(), nums.end(), original);
-        while(it!=nums.end()){
-            original =2 * original;
-             it = std::find(nums.begin(), nums.end(), original);
+        while(true){
+            if(find(nums.begin(),nums.end(),original) != nums.end()){
+                original = 2*original;
+            }
+            else{
+                return original;
+            }
         }
         return original;
     }
