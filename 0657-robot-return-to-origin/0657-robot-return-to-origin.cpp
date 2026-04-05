228e5bc9f803b40289eada81126958afc55c9a41
Sourav <145606223+sksrv@users.noreply.github.com>
2026-04-05 23:38:01 +0530

    Time: 3 ms (46.4%), Space: 10.2 MB (95.43%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,43 +1,16 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int r = 0;
-        int l = 0;
-        int u = 0;
-        int d = 0;
+        int x = 0, y = 0;
 
-        for(char  c : moves){
-            if(c == 'R' && l > 0){
-                l--;
-            }
-            else if(c == 'R' && l <= 0){
-                r++;
-            }
-            else if(c == 'L' && r > 0){
-                r--;
-            }
-            else if(c == 'L' && r <= 0){
-                l++;
-            }
-            else if(c == 'U' && d > 0){
-                d--;
-            }
-            else if(c == 'U' && d <= 0){
-                u++;
-            }
-            else if(c == 'D' && u > 0){
-                u--;
-            }
-            else if(c == 'D' && u <= 0){
-                d++;
-            }
+        for(char c : moves){
+            if(c == 'R') x++;
+            else if(c == 'L') x--;
+            else if(c == 'U') y++;
+            else if(c == 'D') y--;
         }
 
-        if((r == 0) && (l == 0) && (u == 0) && (d == 0)){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return (x == 0 && y == 0);
+
     }
 };
