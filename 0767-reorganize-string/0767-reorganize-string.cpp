ba67a0696d7bdd57cd1dc903b78563f71178af0f
Sourav <145606223+sksrv@users.noreply.github.com>
2026-07-03 11:35:26 +0530

    Time: 0 ms (100%), Space: 8.7 MB (7.15%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     struct cmp {
-        bool operator()(const pair<int, char>& a,
+        bool operator()(const pair<int, char>& a,  
                         const pair<int, char>& b) const {
-            return a.first < b.first;
+            return a.first < b.first;        //max heap
         }
     };
 
     string reorganizeString(string s) {
-        priority_queue<pair<int, char>, vector<pair<int, char>>, cmp> pq;
+        priority_queue<pair<int, char>, vector<pair<int, char>>, cmp> pq;   //max heap
         unordered_map<char, int> mp;
 
         for (char ch : s)
-            mp[ch]++;
+            mp[ch]++;                 //store freq
 
         for (auto it : mp)
-            pq.push({it.second, it.first});
+            pq.push({it.second, it.first});         //store all pair in heap
 
         string res = "";
-        int seat = 0;
+        int seat = 0;                      //index
 
         while (!pq.empty()) {
 
@@ -30,31 +30,31 @@ public:
                 res.push_back(temp.second);
                 seat++;
 
-                temp.first--;
+                temp.first--;          //reduce freq by 1
 
                 if (temp.first > 0)
                     pq.push(temp);
             }
             else {
 
-                pair<int, char> high = pq.top();
-                pq.pop();
+                pair<int, char> high = pq.top();         //if prev element is same
+                pq.pop();                                 
 
                 if (pq.empty())
-                    return "";
+                    return "";                             //if heap is empty
 
-                auto temp = pq.top();
+                auto temp = pq.top();                      //next highest element
                 pq.pop();
 
-                res.push_back(temp.second);
+                res.push_back(temp.second); 
                 seat++;
 
-                temp.first--;
+                temp.first--;                            //reduce freq by 1
 
                 if (temp.first > 0)
-                    pq.push(temp);
+                    pq.push(temp);                    
 
-                pq.push(high);
+                pq.push(high);                            // push highest freq element
             }
         }
 
